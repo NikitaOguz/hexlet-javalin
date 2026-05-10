6a66561e8f98d145ffcdbe3c1c32942ac1beb213
Никита Кобытев <nikitakobytev@mail.ru>
2026-05-10 23:51:27 +0700

    add jte layout

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -7,7 +7,6 @@ import static io.javalin.rendering.template.TemplateUtil.model;
 
 import java.util.List;
 
-import org.example.hexlet.dto.courses.CoursePage;
 import org.example.hexlet.dto.courses.CoursesPage;
 import org.example.hexlet.model.Course;
 
@@ -18,29 +17,17 @@ public class HelloWorld {
             config.fileRenderer(new JavalinJte());
         });
 
-        var courses = List.of(
-                new Course(1L, "Java", "Изучение языка Java"),
-                new Course(2L, "Javalin", "Создание веб-приложений на Javalin")
-        );
-
         app.get("/", ctx -> ctx.redirect("/courses"));
 
         app.get("/courses", ctx -> {
+            var courses = List.of(
+                    new Course("Java", "Изучение Java"),
+                    new Course("Javalin", "Создание веб-приложений")
+            );
+
             var page = new CoursesPage(courses, "Курсы по программированию");
+
             ctx.render("courses/index.jte", model("page", page));
-        });
-
-        app.get("/courses/{id}", ctx -> {
-            var id = Long.valueOf(ctx.pathParam("id"));
-
-            var course = courses.stream()
-                    .filter(c -> c.getId().equals(id))
-                    .findFirst()
-                    .orElseThrow(() -> new RuntimeException("Course not found"));
-
-            var page = new CoursePage(course);
-
-            ctx.render("courses/show.jte", model("page", page));
         });
 
         app.start(7070);
--- app/src/main/java/org/example/hexlet/model/Course.java
+++ app/src/main/java/org/example/hexlet/model/Course.java
@@ -8,14 +8,10 @@ import lombok.ToString;
 @Setter
 @ToString
 public final class Course {
-    private Long id;
-
-    @ToString.Include
     private String name;
     private String description;
 
-    public Course(Long id, String name, String description) {
-        this.id = id;
+    public Course(String name, String description) {
         this.name = name;
         this.description = description;
     }
