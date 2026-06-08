182f8cf80d9d1f1b8dfddf99416f1e828aba193f
Никита Кобытев <nikitakobytev@mail.ru>
2026-06-08 15:41:50 +0700

    two step done

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,56 +1,14 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
-import io.javalin.rendering.template.JavalinJte;
-
-import static io.javalin.rendering.template.TemplateUtil.model;
-
-import java.util.List;
-
-import org.example.hexlet.dto.courses.CoursesPage;
-import org.example.hexlet.dto.users.UserPage;
-import org.example.hexlet.model.Course;
 
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
-            config.fileRenderer(new JavalinJte());
         });
-
-        app.get("/", ctx -> ctx.redirect("/courses"));
-
-        app.get("/courses", ctx -> {
-            var allCourses = List.of(
-                    new Course("Java", "Изучение языка Java"),
-                    new Course("Javalin", "Создание веб-приложений"),
-                    new Course("SQL", "Работа с базами данных")
-            );
-
-            var term = ctx.queryParam("term");
-
-            var courses = allCourses;
-
-            if (term != null && !term.isBlank()) {
-                var normalizedTerm = term.toLowerCase();
-
-                courses = allCourses.stream()
-                        .filter(course ->
-                                course.getName().toLowerCase().contains(normalizedTerm)
-                                        || course.getDescription().toLowerCase().contains(normalizedTerm)
-                        )
-                        .toList();
-            }
-
-            var page = new CoursesPage(
-                    courses,
-                    "Курсы по программированию",
-                    term
-            );
-
-            ctx.render("courses/index.jte", model("page", page));
-        });
-
-        app.start(7070);
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.start(8080);
     }
 }
