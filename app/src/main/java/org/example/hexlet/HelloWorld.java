66ccd7a49c98e485186fa33c7cc4e669ea2fe24f
Никита Кобытев <nikitakobytev@mail.ru>
2026-06-09 19:59:58 +0700

    add /hello

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,11 +4,16 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        var app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
-        });
+        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
         app.get("/users", ctx -> ctx.result("GET /users"));
         app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name");
+            if (name == null) {
+                name = "World";
+            }
+            ctx.result("Hello, " + name + "!");
+        });
         app.start(8080);
     }
 }
