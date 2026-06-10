b6a3e110bc9bcf9b1e24eedecb9e1443a1085e90
Никита Кобытев <nikitakobytev@mail.ru>
2026-06-10 18:33:26 +0700

    add jte (i adding css for beauty)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,8 @@ dependencies {
     implementation("io.javalin:javalin-rendering:6.1.3")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 tasks.test {
--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,18 +1,28 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import static io.javalin.rendering.template.TemplateUtil.model;
+import org.example.hexlet.model.Course;
+import org.example.hexlet.dto.courses.CoursesPage;
+
+import java.util.List;
 
 public class HelloWorld {
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+            config.staticFiles.add("/public");
         });
 
-        // Название параметров мы выбрали произвольно
-        app.get("/users/{id}/post/{postId}", ctx -> {
-            var usersId = ctx.pathParam("id");
-            var postId =  ctx.pathParam("postId");
-            ctx.result("Users ID: " + usersId + " Post ID: " + postId);
+        app.get("/courses", ctx -> {
+            var java = new Course("Java", "ООП");
+            java.setId(1L);
+            var courses = List.of(java);
+                    var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
         });
 
         app.start(7070);
