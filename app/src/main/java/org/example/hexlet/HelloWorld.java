c7f7c3f8318f3c044726764e7bcf9ab768430f25
Никита Кобытев <nikitakobytev@mail.ru>
2026-06-16 18:25:19 +0700

    add user and repository

--- app/src/main/java/org/example/hexlet/HelloWorld.java
+++ app/src/main/java/org/example/hexlet/HelloWorld.java
@@ -3,18 +3,22 @@ package org.example.hexlet;
 import org.example.hexlet.dto.courses.CoursesPage;
 import io.javalin.Javalin;
 import org.example.hexlet.model.Course;
+import org.example.hexlet.model.User;
 import org.example.hexlet.repository.CourseRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
+import io.javalin.rendering.template.JavalinJte;
+import org.example.hexlet.repository.UserRepository;
 
 public class HelloWorld {
     public static void main(String[] args) {
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
 
         app.get("/courses", ctx -> {
@@ -35,6 +39,19 @@ public class HelloWorld {
         var page = new CoursesPage(courses, term, header);
         ctx.render("courses/index.jte", model("page", page));
     });
+        app.get("/users/build", ctx -> {
+            ctx.render("users/build.jte");
+        });
+        app.post("/users", ctx -> {
+            var name = ctx.formParam("name").trim();
+            var email = ctx.formParam("email").trim().toLowerCase();
+            var password = ctx.formParam("password");
+            var passwordConfirmation = ctx.formParam("passwordConfirmation");
+
+            var user = new User(name, email, password);
+            UserRepository.save(user);
+            ctx.redirect("/users");
+        });
         app.start(7070);
 }
 }
